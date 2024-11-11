5cd21dea08ac50995e391d2ab19909fd73f7486b
Net2Fox <trakounet.fox@gmail.com>
2024-11-11 10:52:50 +0300

    Реализовано сохранение времени отправки файла в названии файла

--- EJournalWPF/Data/DataRepository.cs
+++ EJournalWPF/Data/DataRepository.cs
@@ -167,7 +167,10 @@ namespace EJournalWPF.Data
                     string student = null;
                     string subDirectory = null;
                     string fileName = null;
-
+                    if (_saveDateTime)
+                    {
+                        fileName = $"{mail.Date.ToString("dd.MM HH-mm")}";
+                    }
                     group = mail.FromUser.Group.Name;
                     if (!Directory.Exists($"{_saveDirectory}/{group}"))
                     {
@@ -195,7 +198,7 @@ namespace EJournalWPF.Data
 
                     foreach (var file in mail.Files)
                     {
-                        fileName = Regex.Replace(file.Filename, @"[<>:""|?*]", string.Empty);
+                        fileName = $"{fileName} {Regex.Replace(file.Filename, @"[<>:""|?*]", string.Empty)}";
                         if (subDirectory != null)
                         {
                             fileName = $"{_saveDirectory}/{group}/{student}/{subDirectory}/{fileName}";
