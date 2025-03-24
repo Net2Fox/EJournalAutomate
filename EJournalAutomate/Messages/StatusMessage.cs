6e3092377ed762f9ae658c75b40409b448430dc7
Net2Fox <trakounet.fox@gmail.com>
2025-03-24 14:28:23 +0300

    Добавлен StatusMessage для передачи информации о загрузке данных

---
+++ EJournalAutomate/Messages/StatusMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJournalAutomate.Messages
+{
+    public class StatusMessage
+    {
+        public string Message { get; }
+        public bool IsLoading { get; }
+
+        public StatusMessage(string message, bool isLoading)
+        {
+            Message = message;
+            IsLoading = isLoading;
+        }
+    }
+}
