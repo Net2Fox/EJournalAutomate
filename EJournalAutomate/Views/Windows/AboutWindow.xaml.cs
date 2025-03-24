72c20073d0f7a01f09505f1746e5747e7eb27644
Net2Fox <trakounet.fox@gmail.com>
2025-03-24 17:26:26 +0300

    Исправлена ссылка и метод открытия ссылки

--- EJournalAutomate/Views/Windows/AboutWindow.xaml.cs
+++ EJournalAutomate/Views/Windows/AboutWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace EJournalAutomate.Views.Windows
 {
@@ -26,7 +15,7 @@ namespace EJournalAutomate.Views.Windows
 
         private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
         {
-            System.Diagnostics.Process.Start(e.Uri.ToString());
+            Process.Start(new ProcessStartInfo(e.Uri.ToString()) { UseShellExecute = true });
         }
     }
 }
