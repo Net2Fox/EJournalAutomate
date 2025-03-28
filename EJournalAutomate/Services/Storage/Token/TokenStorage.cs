e5228f23d641a8d4bded7d696b627a3369afbc9b
Net2Fox <trakounet.fox@gmail.com>
2025-03-28 09:08:20 +0300

    Сделано логирование в TokenStorage

--- EJournalAutomate/Services/Storage/Token/TokenStorage.cs
+++ EJournalAutomate/Services/Storage/Token/TokenStorage.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using Microsoft.Extensions.Logging;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -7,33 +8,66 @@ namespace EJournalAutomate.Services.Storage.Token
     public class TokenStorage : ITokenStorage
     {
         private readonly string _tokenFilePath = Path.Combine(Environment.CurrentDirectory, "token.dat");
+        private readonly ILogger<TokenStorage> _logger;
+
+        public TokenStorage(ILogger<TokenStorage> logger)
+        {
+            _logger = logger;
+
+            _logger.LogInformation("TokenStorage инициализирован");
+        }
+
         public async Task SaveTokenAsync(string token)
         {
-            byte[] tokenBytes = Encoding.UTF8.GetBytes(token);
+            _logger.LogInformation("Попытка сохранить токен");
+            try
+            {
+                byte[] tokenBytes = Encoding.UTF8.GetBytes(token);
 
-            byte[] encryptedBytes = ProtectedData.Protect(tokenBytes, null, DataProtectionScope.CurrentUser);
+                byte[] encryptedBytes = ProtectedData.Protect(tokenBytes, null, DataProtectionScope.CurrentUser);
 
-            await File.WriteAllBytesAsync(_tokenFilePath, encryptedBytes);
+                await File.WriteAllBytesAsync(_tokenFilePath, encryptedBytes);
 
-            Array.Clear(tokenBytes, 0, tokenBytes.Length);
+                Array.Clear(tokenBytes, 0, tokenBytes.Length);
+            }
+            catch (Exception ex)
+            {
+                var exception = new Exception("Не удалось сохранить токен", ex);
+                _logger.LogCritical(exception, "Не удалось сохранить токен");
+                throw exception;
+            }
+
         }
 
         public async Task<string> LoadTokenAsync()
         {
+            _logger.LogInformation("Попытка прочитать файл с токеном");
+
             if (!File.Exists(_tokenFilePath))
             {
+                _logger.LogInformation("Файл с токеном отсутствует");
                 return string.Empty;
             }
 
-            byte[] encryptedBytes = await File.ReadAllBytesAsync(_tokenFilePath);
+            try
+            {
+                byte[] encryptedBytes = await File.ReadAllBytesAsync(_tokenFilePath);
 
-            byte[] decryptedBytes = ProtectedData.Unprotect(encryptedBytes, null, DataProtectionScope.CurrentUser);
+                byte[] decryptedBytes = ProtectedData.Unprotect(encryptedBytes, null, DataProtectionScope.CurrentUser);
 
-            string authToken = Encoding.UTF8.GetString(decryptedBytes);
+                string authToken = Encoding.UTF8.GetString(decryptedBytes);
 
-            Array.Clear(decryptedBytes, 0, decryptedBytes.Length);
+                Array.Clear(decryptedBytes, 0, decryptedBytes.Length);
 
-            return authToken;
+                return authToken;
+            }
+            catch (Exception ex)
+            {
+                var exception = new Exception("Не удалось прочитать файл с токеном", ex);
+                _logger.LogCritical(exception, "Не удалось прочитать файл с токеном");
+                throw exception;
+            }
+
         }
 
         public bool TokenExists() => File.Exists(_tokenFilePath);
