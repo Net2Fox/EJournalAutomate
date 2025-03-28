4af613a928c93e30eb6cb84a9d5177f5d52ed1bb
Net2Fox <trakounet.fox@gmail.com>
2025-03-28 09:24:19 +0300

    Сделано логирование (сомнительное?) в MainWindowViewModel

--- EJournalAutomate/ViewModels/MainWindowViewModel.cs
+++ EJournalAutomate/ViewModels/MainWindowViewModel.cs
@@ -2,33 +2,58 @@
 using CommunityToolkit.Mvvm.Input;
 using EJournalAutomate.Services.Storage.Settings;
 using EJournalAutomate.Views.Windows;
+using Microsoft.Extensions.Logging;
+using System.Windows;
 
 namespace EJournalAutomate.ViewModels
 {
     public partial class MainWindowViewModel : ObservableObject
     {
         private readonly ISettingsStorage _settingsStorage;
+        private readonly ILogger<MainWindowViewModel> _logger;
 
         [ObservableProperty]
         private bool _saveDate;
 
-        public MainWindowViewModel(ISettingsStorage settingsStorage)
+        public MainWindowViewModel(ISettingsStorage settingsStorage, ILogger<MainWindowViewModel> logger)
         {
             _settingsStorage = settingsStorage;
+            _logger = logger;
+
             InitializeAsync();
         }
 
         private async void InitializeAsync()
         {
-            await _settingsStorage.LoadSettings();
-            SaveDate = _settingsStorage.SaveDate;
+            try
+            {
+                await _settingsStorage.LoadSettings();
+                SaveDate = _settingsStorage.SaveDate;
+                _logger.LogDebug("MainWindowViewModel инициализирована");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                _logger.LogCritical(ex, "MainWindowViewModel не инициализирована");
+            }
+
         }
 
         [RelayCommand]
         private async Task ToggleSaveDate()
         {
-            _settingsStorage.SetSaveDate(SaveDate);
-            await _settingsStorage.SaveSettings();
+            try
+            {
+                _settingsStorage.SetSaveDate(SaveDate);
+                await _settingsStorage.SaveSettings();
+                _logger.LogInformation($"Настройка даты изменена: {SaveDate}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                _logger.LogError(ex, $"Настройка даты не изменена");
+            }
+
         }
 
         [RelayCommand]
@@ -36,6 +61,7 @@ namespace EJournalAutomate.ViewModels
         {
             AboutWindow aboutWindow = new AboutWindow();
             aboutWindow.ShowDialog();
+            _logger.LogInformation("Окно \"О программе\" успешно открыто");
         }
     }
 }
