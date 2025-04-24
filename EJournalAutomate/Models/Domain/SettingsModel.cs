db9e74540e177fd328284c1be3394bcc1bec66bb
Net2Fox <trakounet.fox@gmail.com>
2025-04-24 14:27:53 +0300

    Добавлена модель настроек

---
+++ EJournalAutomate/Models/Domain/SettingsModel.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace EJournalAutomate.Models.Domain
+{
+    public class SettingsModel
+    {
+        public string SavePath { get; set; } = Path.Combine(Environment.CurrentDirectory, "Письма");
+        public bool SaveDate { get; set; } = false;
+        public bool SaveLogs { get; set; } = false;
+        public bool SaveFullName { get; set; } = true;
+        public string Vendor { get; set; } = string.Empty;
+
+
+    }
+}
