336d770559de280d342ce89c9468bcc6c427e6dc
Net2Fox <trakounet.fox@gmail.com>
2026-04-09 22:01:47 +0300

    Добавлено окно для ввода DevKey

---
+++ EJournalAutomate/Views/Windows/DevKeySettingWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace EJournalAutomate.Views.Windows;
+
+public partial class DevKeySettingWindow : Window
+{
+    public string DevKey
+    {
+        get => DevKeyTextBox.Text;
+        set
+        {
+            DevKeyTextBox.Text = value;
+        }
+    }
+    
+    public DevKeySettingWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void SetDevKeyButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(DevKey))
+        {
+            this.DialogResult = true;
+        }
+    }
+}
