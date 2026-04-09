3eab933b766de93043b9fb4178a511b883da6587
Net2Fox <trakounet.fox@gmail.com>
2026-04-09 22:07:58 +0300

    Реализован DialogService для показа MessageBox

---
+++ EJournalAutomate/Services/Dialog/DialogService.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace EJournalAutomate.Services.Dialog;
+
+public class DialogService : IDialogService
+{
+    public void ShowError(string message)
+    {
+        MessageBox.Show(message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+    }
+
+    public void ShowInfo(string message)
+    {
+        MessageBox.Show(message, "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+
+    public bool ShowConfirmation(string message)
+    {
+        return MessageBox.Show(message, "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes;
+    }
+}
---
+++ EJournalAutomate/Services/Dialog/IDialogService.cs
@@ -0,0 +1,8 @@
+﻿namespace EJournalAutomate.Services.Dialog;
+
+public interface IDialogService
+{
+    void ShowError(string message);
+    void ShowInfo(string message);
+    bool ShowConfirmation(string message);
+}
