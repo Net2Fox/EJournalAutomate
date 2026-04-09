4b85f2b24ce0d052d0d9ebe542482789411c72fd
Net2Fox <trakounet.fox@gmail.com>
2026-04-09 22:02:34 +0300

    В MainWindow добавлена проверка наличия DevKey

--- EJournalAutomate/Views/MainWindow.xaml.cs
+++ EJournalAutomate/Views/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using EJournalAutomate.Services.Navigation;
 using EJournalAutomate.ViewModels;
 using EJournalAutomate.Views.Pages;
 using System.Windows;
+using EJournalAutomate.Services.Storage.Token;
+using EJournalAutomate.Services.Window;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EJournalAutomate.Views
@@ -15,19 +17,39 @@ namespace EJournalAutomate.Views
     {
         private readonly INavigationService _navigationService;
         private readonly IAPIService _apiService;
+        private readonly IWindowService _windowService;
+        private readonly ISecureStorage _devKeyStorage;
         
-        public MainWindow(MainWindowViewModel viewModel, INavigationService navigationService, IAPIService apiService)
+        public MainWindow(
+            MainWindowViewModel viewModel, 
+            INavigationService navigationService, 
+            IAPIService apiService, 
+            IWindowService windowService,
+            [FromKeyedServices("devkey")]ISecureStorage devKeyStorage)
         {
             InitializeComponent();
             DataContext = viewModel;
             _navigationService = navigationService;
             _apiService = apiService;
+            _windowService = windowService;
+            _devKeyStorage = devKeyStorage;
         }
 
         private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             _navigationService.SetFrame(MainFrame);
 
+            bool devKeyExists = await _apiService.LoadDevKeyFromAsync();
+            if (!devKeyExists)
+            {
+                var result = _windowService.ShowDevKeySettingWindow();
+                if (result != null)
+                {
+                    await _devKeyStorage.SaveAsync(result);
+                    await _apiService.LoadDevKeyFromAsync();
+                }
+            }
+            
             bool tokenExists = await _apiService.LoadTokenFromAsync();
             if (tokenExists)
             {
