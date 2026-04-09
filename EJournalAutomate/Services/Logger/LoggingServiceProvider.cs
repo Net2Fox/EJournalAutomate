e54f935044272108df978af8851fc0cba1ff778d
Net2Fox <trakounet.fox@gmail.com>
2026-04-09 21:30:57 +0300

    В LoggingServiceProvider внедряется IOptionsMonitor и отслеживает изменения настройки сохранения логов

--- EJournalAutomate/Services/Logger/LoggingServiceProvider.cs
+++ EJournalAutomate/Services/Logger/LoggingServiceProvider.cs
@@ -1,17 +1,30 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using EJournalAutomate.Models.Domain;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace EJournalAutomate.Services.Logger
 {
     public class LoggingServiceProvider : ILoggerProvider
     {
         private readonly string _logFilePath;
+        private readonly IDisposable? _onChangeListener;
 
-        public LoggingServiceProvider(string path)
+        public LoggingServiceProvider(string path, IOptionsMonitor<SettingsModel> settingsMonitor)
         {
             _logFilePath = path;
+            LoggingService.SetSettingsSaveLogs(settingsMonitor.CurrentValue.SaveLogs);
+
+            _onChangeListener = settingsMonitor.OnChange(settings =>
+            {
+                LoggingService.SetSettingsSaveLogs(settings.SaveLogs);
+            });
         }
 
         public ILogger CreateLogger(string categoryName) => new LoggingService(_logFilePath, categoryName);
-        public void Dispose() { }
+
+        public void Dispose()
+        {
+            _onChangeListener?.Dispose();
+        }
     }
 }
