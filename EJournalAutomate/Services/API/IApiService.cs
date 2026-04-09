10d6bf14cc87c8e8ceb2174a6ee1ba1df9e9fd42
Net2Fox <trakounet.fox@gmail.com>
2026-04-09 21:35:20 +0300

    В IAPIService добавлен LoadDevKeyFromAsync

--- EJournalAutomate/Services/API/IApiService.cs
+++ EJournalAutomate/Services/API/IApiService.cs
@@ -5,6 +5,7 @@ namespace EJournalAutomate.Services.API
     public interface IAPIService
     {
         Task<bool> LoadTokenFromAsync();
+        Task<bool> LoadDevKeyFromAsync();
         Task AuthenticateAsync(string login, string password, string vendor);
         Task<List<Message>> GetMessagesAsync(int limit = 20);
         Task<MessageInfo> GetMessageInfoAsync(string id);
